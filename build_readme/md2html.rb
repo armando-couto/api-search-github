14f466f0b40fd437d228b768609e3b7b8381ff11
Armando Couto <coutoarmando@gmail.com>
2019-07-31 13:45:14 -0300

    Corrige o CSS do README.html

--- build_readme/md2html.rb
+++ build_readme/md2html.rb
@@ -3,10 +3,10 @@ require 'erb'
 
 if ARGV.length >= 2
 
-  iFile = File.new(ARGV[0], "rb") 
+  iFile = File.new(ARGV[0], "rb")
 
-  md = iFile.read.gsub( %r{img:[^\s<]+} ) do |img|
-    if img[/(?:png|jpe?g|gif)$/]  
+  md = iFile.read.gsub(%r{img:[^\s<]+}) do |img|
+    if img[/(?:png|jpe?g|gif)$/]
       "<img src='img/#{img.slice(4..-1)}' />"
     else
       "#{img}"
@@ -14,12 +14,12 @@ if ARGV.length >= 2
   end
 
   md.gsub!('_', '\_')
-  
+
   html = RedcarpetCompat.new(md, :hard_wrap, :autolink, :fenced_code, :tables, :smart).to_html
 
-  html.gsub!(%r[<a\b.*?</a>]i) {|m| m.gsub('\_' , '_') }
-  html.gsub!(%r[<img\b.*?>]i) {|m| m.gsub('\_' , '_') }
-  html.gsub!(/<code>.*?<\/code>/m) {|m| m.gsub('\_' , '_') }
+  html.gsub!(%r[<a\b.*?</a>]i) {|m| m.gsub('\_', '_')}
+  html.gsub!(%r[<img\b.*?>]i) {|m| m.gsub('\_', '_')}
+  html.gsub!(/<code>.*?<\/code>/m) {|m| m.gsub('\_', '_')}
 
   template = ERB.new File.new(ARGV[2] == nil ? "md2html.erb" : ARGV[2]).read, nil, "%"
   template.result(binding)
@@ -30,8 +30,7 @@ if ARGV.length >= 2
   iFile.close
   oFile.close
 
-else 
+else
 
   puts "Usage: ruby md2html.rb input.rb output.html\n"
-
 end
