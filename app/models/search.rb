150fe8ca50e4a1f733fb70d60ceae28b86dc4873
Armando Couto <coutoarmando@gmail.com>
2019-08-01 06:58:31 -0300

    Conclui a API repositories

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,30 +1,36 @@
 class Search
-  attr_accessor :full_name, :description, :star_count, :amount_of_forks, :author_name
+  attr_accessor :id, :full_name, :description, :star_count, :amount_of_forks, :author_name
+
+  QUANTITY_PER_PAGE = 15
 
   def initialize(item)
+    @id = item["id"]
     @full_name = item["full_name"]
     @description = item["description"]
-    @star_count = count item["stargazers_url"]
-    @amount_of_forks = count item["forks_url"]
+    # @star_count = count item["stargazers_url"]
+    # @amount_of_forks = count item["forks_url"]
     @author_name = item["owner"]["login"]
   end
 
-  def self.repositories
-    response = HTTParty.get("https://api.github.com/repositories",
-                            {headers: {
-                                "User-Agent" => "Httparty",
-                                "Content-Type" => "application/json",
-                                "Authorization" => "token b0d0bc7366b3632e3a18f410a7625f8cead8cdf9"}
-                            })
-    list = []
-    response.each do |item|
-      ## TODO Limit per page 15
-      list << Search.new(item)
-    end
-    list
+  # https://developer.github.com/v3/repos/#list-all-public-repositories
+  def self.repositories oauth_token, next_id
+    self.pagination HTTParty.get("https://api.github.com/repositories?since=#{next_id}",
+                                 {headers: {
+                                     "User-Agent" => "Httparty",
+                                     "Content-Type" => "application/json",
+                                     "Authorization" => "token #{oauth_token}"}
+                                 })
   end
 
   private
+
+  def self.pagination response
+    list = []
+    response.each_with_index do |item, index|
+      return list unless (index + 1) != QUANTITY_PER_PAGE
+      list << Search.new(item)
+    end
+  end
 
   def count url
     response = HTTParty.get(url)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 puts "Populando o banco de dados"
 
-User.create!({name: "armando", password: "123456", oauth_token: "b0d0bc7366b3632e3a18f410a7625f8cead8cdf9"})
+User.create!({name: "armando", password: "123456", oauth_token: "fd171065f71eefb8d7d94eace6a8ddf50dd97db2"})
